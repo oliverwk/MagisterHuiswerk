2d2594e2272fdac255edad8ed5d914eb294b53ce
øllen <30601311+oliverwk@users.noreply.github.com>
2021-05-17 16:06:55 +0200

    first commit

--- MagisterHuiswerk Extension/Resources/content.js
+++ MagisterHuiswerk Extension/Resources/content.js
@@ -5,3 +5,101 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+consoe.log("Hello, world");
+
+const selector ="#agenda-afspraak-bewerken-container > section > div > div.widget.wide.new-appointment-block > fieldset > ul";
+
+class Dealine {
+  constructor() {
+      let li = document.createElement("li");
+      let label = document.createElement("label");
+      label.innerHTML = "Dealine";
+      li.appendChild(label);
+      let div = document.createElement("div");
+      li.appendChild(div);
+      
+      let Kspan = document.createElement("span");
+      Kspan.className = "k-widget k-datepicker k-header ng-binding";
+      div.appendChild(Kspan);
+      
+      let KPspan = document.createElement("span");
+      KPspan.className = "k-picker-wrap k-state-default";
+      Kspan.appendChild(KPspan);
+      
+      let input = document.createElement("input");
+      input.className = "ng-binding k-input";
+      input.setAttribute("type", "date");
+      input.setAttribute("id", "calendar");
+      input.setAttribute("data-k-options", "agendaCalendarOptions");
+      input.setAttribute("data-ng-bind", "calendarSelectedDate");
+      input.setAttribute("data-k-on-change", "calendarOnChange(kendoEvent)");
+      input.setAttribute("kendo-date-picker", "agendaCalendar");
+      input.setAttribute("data-role", "datepicker");
+      input.setAttribute("role", "combobox");
+      input.setAttribute("aria-expanded", "false");
+      input.setAttribute("aria-owns", "calendar_dateview");
+      input.setAttribute("aria-disabled", "false");
+      input.setAttribute("aria-readonly", "false");
+      input.style.width = "100%";
+      KPspan.appendChild(input);
+      
+      
+      let Cspan = document.createElement("span");
+      Cspan.className = "check";
+      div.appendChild(Cspan);
+      
+      let Cinput = document.createElement("input");
+      Cinput.setAttribute("type", "checkbox");
+      Cinput.className = "checkbox ng-pristine ng-untouched ng-valid ng-empty";
+      Cinput.setAttribute("name", "voordelesaf");
+      Cinput.setAttribute("data-ng-model", "afspraak.VoorDeLesAf");
+      Cinput.setAttribute("id", "voordelesaf");
+      Cinput.setAttribute("tabindex", "4");
+      Cspan.appendChild(Cinput);
+      
+      let Vlabel = document.createElement("label");
+      Vlabel.setAttribute("for", "voordelesaf");
+      Vlabel.innerText = "voor de les af";
+      Vlabel.innerHTML = "<span></span>voor de les af";
+      Cspan.appendChild(Vlabel);
+      
+      return li;
+  }
+}
+
+function GetElement() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let theIframe = document.querySelector(selector);
+            resolve(theIframe);
+            console.log("ThisIsTheElement:", theIframe);
+        } catch (err) {
+            if (e instanceof TypeError) {
+                console.error("[ERROR] Whe coun't find the elemnt", err)
+                reject(err);
+             } else {
+                 console.error("[ERROR] An error accourd in the querySelector", err)
+                 reject(err);
+            }
+        }
+    });
+}
+
+function AddDeadlineElement(list) {
+    let DealineElement = new Dealine();
+    list.appendChild(DealineElement);
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        let list = await GetElement();
+        AddDeadlineElement(list);
+    } catch (err) {
+        console.error("[ERROR] At DOMContentLoaded", err);
+    }
+});
+
+/*(async function () {
+    
+})();*/
+
